6477d12a3f034429924049213ef4f269c45d1c08
artazidele <77989249+artazidele@users.noreply.github.com>
2021-08-28 19:45:27 +0300

    building lists and navigation

--- SwiftUITutorial/Views/ContentView.swift
+++ SwiftUITutorial/Views/ContentView.swift
@@ -10,35 +10,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Title Text")
-                    .font(.title)
-                
-                HStack {
-                    Text("This is left subheadline")
-                    Spacer()
-                    Text("This is right subheadline")
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                Text("This is title2")
-                    .font(.title2)
-                Text("This is the text at bottom")
-                
-            }
-            .padding()
-            
-            Spacer()
+//            MapView()
+//                .ignoresSafeArea(edges: .top)
+//                .frame(height: 300)
+//
+//            CircleImage()
+//                .offset(y: -130)
+//                .padding(.bottom, -130)
+//
+//            VStack(alignment: .leading) {
+//                Text("Title Text")
+//                    .font(.title)
+//
+//                HStack {
+//                    Text("This is left subheadline")
+//                    Spacer()
+//                    Text("This is right subheadline")
+//                }
+//                .font(.subheadline)
+//                .foregroundColor(.secondary)
+//
+//                Divider()
+//                Text("This is title2")
+//                    .font(.title2)
+//                Text("This is the text at bottom")
+//                
+//            }
+//            .padding()
+//
+//            Spacer()
         }
     }
 }
